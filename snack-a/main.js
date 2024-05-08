c8e8e0296804b5a7973eed884fb2b49126151c39
federicoalagna <federicoalagna01@gmail.com>
2024-05-08 16:40:52 +0200

    snack-a

---
+++ snack-a/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function contaElementi(array) {
+    return array.length;
+}
+
+
+const array1 = [1, 2, 3, 4, 5];
+console.log(contaElementi(array1)); 
+
+
+const array2 = ['a', 'b', 'c', 'd'];
+console.log(contaElementi(array2)); 
